b6acb823d39a3de83af0172982fc35bf7404cecb
lxiang <liang.xiang@thoughtworks.com>
2023-11-03 19:51:58 +0800

    add loading for login button

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -14,6 +14,7 @@ import {
   Flex,
   Text,
 } from '@radix-ui/themes'
+import { Skeleton } from '@/app/components'
 
 const NavBar = () => {
   return (
@@ -63,7 +64,7 @@ const NavLinks = () => {
 const AuthStatus = () => {
   const { status, data: session } = useSession()
 
-  if (status === 'loading') return null
+  if (status === 'loading') return <Skeleton width="3rem" />
 
   if (status === 'unauthenticated')
     return (
