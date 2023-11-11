fdfd0cadfe199876fd5d911da8780169f1aae38b
lxiang-tw <liang.xiang@thoughtworks.com>
2023-11-12 04:36:45 +0800

    Refactor AssigneeSelect component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -3,49 +3,42 @@
 import { Issue, User } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
 import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { useQuery } from '@tanstack/react-query'
 import { Skeleton } from '@/app/components'
+import { useQuery } from '@tanstack/react-query'
 import toast, { Toaster } from 'react-hot-toast'
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
-    queryKey: ['users'],
-    queryFn: () => fetch('/api/users').then((res) => res.json()),
-    staleTime: 60 * 1000,
-    retry: 3,
-  })
+  const { data: users, error, isLoading } = useUsers()
 
   if (isLoading) return <Skeleton />
 
   if (error) return null
 
+  const assignIssue = (userId: string) => {
+    // axios
+    //   .patch(`/api/issues/${issue.id}`, {
+    //     assignedToUserId: userId === 'unassigned' ? null : userId,
+    //   })
+    //   .catch(() => toast.error('Changes could not be saved.'))
+
+    fetch(`/api/issues/${issue.id}`, {
+      method: 'PATCH',
+      body: JSON.stringify({
+        assignedToUserId: userId === 'unassigned' ? null : userId,
+      }),
+    }).then((res) => {
+      if (!res.ok) toast.error('Changes could not be saved.')
+    })
+    // .catch(() => {
+    //   toast.error('host not found, no connection, server not responding, etc...')
+    // })
+  }
+
   return (
     <>
       <Select.Root
         defaultValue={issue.assignedToUserId || 'unassigned'}
-        onValueChange={(userId) => {
-          // axios
-          //   .patch(`/apii/issues/${issue.id}`, {
-          //     assignedToUserId: userId === 'unassigned' ? null : userId,
-          //   })
-          //   .catch(() => toast.error('Changes could not be saved.'))
-          fetch(`/apii/issues/${issue.id}`, {
-            method: 'PATCH',
-            body: JSON.stringify({
-              assignedToUserId: userId === 'unassigned' ? null : userId,
-            }),
-          }).then((res) => {
-            if (!res.ok) toast.error('Changes could not be saved.')
-          })
-          // .catch(() => {
-          //   toast.error('host not found, no connection, server not responding, etc...')
-          // })
-        }}
+        onValueChange={assignIssue}
       >
         <Select.Trigger placeholder="Assign..." />
         <Select.Content>
@@ -64,5 +57,13 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     </>
   )
 }
+
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => fetch('/api/users').then((res) => res.json()),
+    staleTime: 60 * 1000,
+    retry: 3,
+  })
 
 export default AssigneeSelect
