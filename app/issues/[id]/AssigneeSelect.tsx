3811e2061c2d52373ce027f852d66f4b48fb7042
lxiang-tw <liang.xiang@thoughtworks.com>
2023-11-12 04:22:25 +0800

    Show toast notifications

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -6,6 +6,7 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { Skeleton } from '@/app/components'
+import toast, { Toaster } from 'react-hot-toast'
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
@@ -24,33 +25,43 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   if (error) return null
 
   return (
-    <Select.Root
-      defaultValue={issue.assignedToUserId || 'unassigned'}
-      onValueChange={(userId) => {
-        // axios.patch('/api/issues/' + issue.id, {
-        //   assignedToUserId: userId === 'unassigned' ? null : userId,
-        // })
-        fetch(`/api/issues/${issue.id}`, {
-          method: 'PATCH',
-          body: JSON.stringify({
-            assignedToUserId: userId === 'unassigned' ? null : userId,
-          }),
-        })
-      }}
-    >
-      <Select.Trigger placeholder="Assign..." />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="unassigned">Unassigned</Select.Item>
-          {users?.map((user) => (
-            <Select.Item key={user.id} value={user.id}>
-              {user.name}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        defaultValue={issue.assignedToUserId || 'unassigned'}
+        onValueChange={(userId) => {
+          // axios
+          //   .patch(`/apii/issues/${issue.id}`, {
+          //     assignedToUserId: userId === 'unassigned' ? null : userId,
+          //   })
+          //   .catch(() => toast.error('Changes could not be saved.'))
+          fetch(`/apii/issues/${issue.id}`, {
+            method: 'PATCH',
+            body: JSON.stringify({
+              assignedToUserId: userId === 'unassigned' ? null : userId,
+            }),
+          }).then((res) => {
+            if (!res.ok) toast.error('Changes could not be saved.')
+          })
+          // .catch(() => {
+          //   toast.error('host not found, no connection, server not responding, etc...')
+          // })
+        }}
+      >
+        <Select.Trigger placeholder="Assign..." />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value="unassigned">Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
   )
 }
 
