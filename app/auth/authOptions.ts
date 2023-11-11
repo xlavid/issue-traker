cd342cef6c2166a4d07d92a8bc62ea474e324852
lxiang-tw <liang.xiang@thoughtworks.com>
2023-11-12 02:27:31 +0800

    modify auth options

--- app/auth/authOptions.ts
+++ app/auth/authOptions.ts
@@ -1,7 +1,9 @@
-import GoogleProvider from 'next-auth/providers/google'
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import prisma from '@/prisma/client'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { NextAuthOptions } from 'next-auth'
+import GitHubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+import SlackProvider from 'next-auth/providers/slack'
 
 const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -9,18 +11,27 @@ const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      authorization: {
-        params: {
-          prompt: 'consent',
-          access_type: 'offline',
-          response_type: 'code',
-        },
-      },
+      // authorization: {
+      //   params: {
+      //     prompt: 'consent',
+      //     access_type: 'offline',
+      //     response_type: 'code',
+      //   },
+      // },
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    SlackProvider({
+      clientId: process.env.SLACK_CLIENT_ID!,
+      clientSecret: process.env.SLACK_CLIENT_SECRET!,
     }),
   ],
-  session: {
-    strategy: 'jwt',
-  },
+  // session: {
+  //   strategy: 'jwt',
+  // },
+  // secret: process.env.NEXTAUTH_SECRET,
 }
 
 export default authOptions
