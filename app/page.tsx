484cd6b2a0869210cb848d945c9311149deff21d
lxiang-tw <liang.xiang@thoughtworks.com>
2023-11-12 18:21:27 +0800

    add chart and layout the dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import prisma from '@/prisma/client'
 import IssueSummary from './IssueSummary'
 import LatestIssues from './LatestIssues'
+import IssueChart from './IssueChart'
+import { Flex, Grid } from '@radix-ui/themes'
 
 export default async function Home() {
   const open = await prisma.issue.count({
@@ -15,8 +17,13 @@ export default async function Home() {
 
   return (
     <>
-      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
-      <LatestIssues />
+      <Grid columns={{ initial: '1', md: '2' }} gap="5">
+        <Flex direction="column" gap="5">
+          <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+          <IssueChart open={open} inProgress={inProgress} closed={closed} />
+        </Flex>
+        <LatestIssues />
+      </Grid>
     </>
   )
 }
