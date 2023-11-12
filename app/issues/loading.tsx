4610dfb913bbcc6b8af47d4c6bbdab9426d6917b
lxiang-tw <liang.xiang@thoughtworks.com>
2023-11-12 12:23:12 +0800

    Build the Filter Component

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,6 +1,6 @@
 import { Table } from '@radix-ui/themes'
 import { Skeleton } from '@/app/components'
-import IssueActions from './IssueActions'
+import IssueActions from './list/IssueActions'
 
 const LoadingIssuesPage = () => {
   const issues = [1, 2, 3, 4, 5]
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,7 @@
 import prisma from '@/prisma/client'
 import { Table } from '@radix-ui/themes'
 import { IssueStatusBadge, Link } from '@/app/components'
-import IssueActions from './IssueActions'
+import IssueActions from './list/IssueActions'
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany()
