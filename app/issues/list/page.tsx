0dcb0edd1dcfa2073554e9cffb5a7ae3a4ba5aeb
lxiang-tw <liang.xiang@thoughtworks.com>
2023-11-12 18:07:49 +0800

    add pagination, extract issues table

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -2,109 +2,50 @@ import { IssueStatusBadge, Link } from '@/app/components'
 import prisma from '@/prisma/client'
 import { Issue, Status } from '@prisma/client'
 import { ArrowDownIcon, ArrowUpIcon } from '@radix-ui/react-icons'
-import { Table } from '@radix-ui/themes'
+import { Flex, Table } from '@radix-ui/themes'
 import NextLink from 'next/link'
 import IssueActions from './IssueActions'
+import IssueTable, { IssueQuery, columnNames } from './IssueTable'
+import Pagination from '@/app/components/Pagination'
 
 interface Props {
-  searchParams: { status: Status; orderBy: keyof Issue; sort: string }
+  searchParams: IssueQuery
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
-  const columns: {
-    label: string
-    value: keyof Issue
-    className?: string
-  }[] = [
-    { label: 'Issue', value: 'title' },
-    {
-      label: 'Status',
-      value: 'status',
-      className: 'hidden md:table-cell',
-    },
-    {
-      label: 'Created',
-      value: 'createdAt',
-      className: 'hidden md:table-cell',
-    },
-  ]
-
   const statuses = Object.values(Status)
 
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined
 
-  const orderBy = columns
-    .map((column) => column.value)
-    .includes(searchParams.orderBy)
+  const where = { status }
+  const orderBy = columnNames.includes(searchParams.orderBy)
     ? { [searchParams.orderBy]: searchParams.sort }
     : undefined
 
+  const page = parseInt(searchParams.page) || 1
+  const pageSize = 10
+
   const issues = await prisma.issue.findMany({
-    where: {
-      status,
-    },
+    where,
     orderBy,
+    skip: (page - 1) * pageSize,
+    take: pageSize,
   })
 
-  const changeSort = () => {
-    if (searchParams.sort === 'asc') return 'desc'
-    if (searchParams.sort === 'desc') return 'asc'
-    return 'desc'
-  }
+  const issueCount = await prisma.issue.count({ where })
+
   return (
-    <>
+    <Flex direction="column" gap="3">
       <IssueActions />
-      <Table.Root variant="surface">
-        <Table.Header>
-          <Table.Row>
-            {columns.map((column) => (
-              <Table.ColumnHeaderCell
-                key={column.value}
-                className={column.className}
-              >
-                <NextLink
-                  href={{
-                    query: {
-                      ...searchParams,
-                      orderBy: column.value,
-                      sort: changeSort(),
-                    },
-                  }}
-                >
-                  {column.label}
-                </NextLink>
-                {searchParams.sort === 'desc' && (
-                  <ArrowDownIcon className="inline" />
-                )}
-                {searchParams.sort === 'asc' && (
-                  <ArrowUpIcon className="inline" />
-                )}
-              </Table.ColumnHeaderCell>
-            ))}
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {issues.map((issue) => (
-            <Table.Row key={issue.id}>
-              <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-                <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status} />
-                </div>
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                {issue.createdAt.toDateString()}
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
-    </>
+      <IssueTable searchParams={searchParams} issues={issues} />
+      <Pagination
+        pageSize={pageSize}
+        currentPage={page}
+        itemCount={issueCount}
+      />
+    </Flex>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-    <div>hello world</div>
-  )
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string }
+}) {
+  return null
 }
