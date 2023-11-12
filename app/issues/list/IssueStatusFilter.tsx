535f505bf2c805ecc4742e11e9b96c12d5f87b2a
lxiang-tw <liang.xiang@thoughtworks.com>
2023-11-12 16:24:32 +0800

    making columns sortable

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -2,7 +2,7 @@
 
 import { Status } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
 
 const statuses: { label: string; value?: Status }[] = [
@@ -14,18 +14,26 @@ const statuses: { label: string; value?: Status }[] = [
 
 const IssueStatusFilter = () => {
   const router = useRouter()
+  const searchParams = useSearchParams()
 
   return (
     <Select.Root
+      defaultValue={searchParams.get('status') || ''}
       onValueChange={(status) => {
-        const query = status ? `?status=${status}` : ''
+        const params = new URLSearchParams()
+        if (status) params.append('status', status)
+        if (searchParams.get('orderBy'))
+          params.append('orderBy', searchParams.get('orderBy')!)
+        if (searchParams.get('sort'))
+          params.append('sort', searchParams.get('sort')!)
+        const query = params.size ? '?' + params.toString() : ''
         router.push('/issues/list' + query)
       }}
     >
       <Select.Trigger placeholder="Filter by status..." />
       <Select.Content>
         {statuses.map((status) => (
-          <Select.Item key={status.value} value={status.value || 'ALL'}>
+          <Select.Item key={status.label} value={status.value || 'ALL'}>
             {status.label}
           </Select.Item>
         ))}
