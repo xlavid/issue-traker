41b1d003e5b99ceef1f05420ac6190978aa68870
lxiang-tw <liang.xiang@thoughtworks.com>
2023-11-12 15:59:24 +0800

    issue list page url change

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -34,7 +34,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
       setSubmitting(true)
       if (issue) await axios.patch('/api/issues/' + issue.id, data)
       else await axios.post('/api/issues', data)
-      router.push('/issues')
+      router.push('/issues/list')
       router.refresh()
     } catch (error) {
       setSubmitting(false)
@@ -68,7 +68,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button disabled={isSubmitting}>
-          {issue ? 'Update Issue' : 'Submit New Issue'}{' '}
+          {issue ? 'Update Issue' : 'Submit New Issue'}
           {isSubmitting && <Spinner />}
         </Button>
       </form>
